b80525661b9bb412672bcfb579f5052a051be7bf
Dominique Brisco <dbrisco93@gmail.com>
2020-08-06 08:21:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -12,6 +12,8 @@ def link
   puts "It's Dangerous To Go Alone! Take This."
 end
 
-def all_phrases
-  
+def all_phrases(phrase1, phrase2, phrase3, phrase4)
+  phrase1 = Mario
+  phrase2 = toadstool
+  phrase3 = link
 end
